fb1d0802fc2ad1fb3d2294fc80f0717936842bb9
Renan <renan.bezerra@ccc.ufcg.edu.br>
2022-07-09 18:04:52 -0300

    Implementação inicial de todo Menu

--- jogo_da_vida_parte1.hs
+++ jogo_da_vida_parte1.hs
@@ -10,8 +10,14 @@ type Jogadores = [Jogador]
 type Nome = String
 type Pontuacao = Int
 type Tabela = [Char]
-data Jogador = Jogador Nome Pontuacao
-                       deriving (Show, Read)
+data Jogador = Jogador Nome Pontuacao deriving (Show, Read)
+
+
+getString :: String -> IO String
+getString str = do
+                putStr str
+                res <- getLine
+                return res
 
 
 -- Função que inicia o programa
@@ -37,7 +43,6 @@ inicio = do
             else ioError erro
 
 
-
 -- Função que exibe o menu
 menu :: Jogadores -> IO Jogadores
 menu dados = do
@@ -54,9 +59,70 @@ menu dados = do
 
 
 executaOpcao :: Jogadores -> Char -> IO Jogadores
+executaOpcao dados '1' = cadastraJogador dados
+executaOpcao dados '2' = preparaJogo dados
 executaOpcao dados '0' = do
-                            putStrLn ("\nAté uma próxim jogatina :)\n")
+                            putStrLn ("\nAté uma próxima jogatina :)\n")
                             return dados
 executaOpcao dados _ = do
                             putStrLn ("\nOpção inválida :(\n")
+                            putStrLn ("\nPressione um <Enter> para voltar ao Menu\n")
+                            getChar
                             menu dados
+
+
+
+cadastraJogador :: Jogadores -> IO Jogadores
+cadastraJogador dados = do
+                        nome <- getString "\nDigite o nome do jogador: "
+                        if (existeJogador dados nome) then do
+                                                            putStrLn "\nEsse jogador já existe"
+                                                            putStr "\nPressione <Enter> para continuar"
+                                                            getChar
+                                                            menu dados
+                        else do
+                                arq <- openFile "dados.txt" WriteMode
+                                hPutStrLn arq (show ((Jogador nome 0): dados))
+                                hClose arq
+                                putStrLn ("\nUsuário " ++ nome ++ " cadastrado com sucesso")
+                                putStr "\nPressione <Enter> para continuar"
+                                getChar
+                                menu ((Jogador nome 0): dados)
+
+
+
+-- Função que verifica se um jogador já existe. O nome é único.
+existeJogador :: Jogadores -> Nome -> Bool
+existeJogador [] _ = False
+existeJogador ((Jogador n p):xs) nome
+            | (n == nome) = True
+            | otherwise = existeJogador xs nome
+
+
+
+-- Função que prepara o início do jogo
+preparaJogo :: Jogadores -> IO Jogadores
+preparaJogo dados = do
+                    jogador <- getString "\nDigite o nome do jogador: "
+                    -- Testa se o jogador existe
+                    if not (existeJogador dados jogador) then do
+                        putStrLn "\nJogador inexistente!"
+                        putStr "\nPressione <Enter> para continuar"
+                        getChar
+                        menu dados
+                    else do
+                         novoJogo dados jogador
+
+
+
+-- Função que inicia novo jogo
+novoJogo :: Jogadores -> Nome -> IO Jogadores
+novoJogo dados jogador = do
+                            putStrLn ("\nIniciando o jogo")
+                            rodaJogo dados (repeat 'c') jogador
+
+
+
+-- Função que exibe a tabela
+rodaJogo :: Jogadores -> Tabela -> Nome -> IO Jogadores
+rodaJogo dados tabela jogador = menu dados
